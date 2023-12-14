b137e45e4929497d8c32e13032146e0d706ee4ad
NickN1994 <nickneggers@hotmail.com>
2023-12-14 16:41:42 +0100

    aan remotecontroller gewerkt

--- src/main/java/nl/novi/techiteasyhw/controller/RemoteControllerController.java
+++ src/main/java/nl/novi/techiteasyhw/controller/RemoteControllerController.java
@@ -1,12 +1,13 @@
 package nl.novi.techiteasyhw.controller;
 
 import nl.novi.techiteasyhw.dto.RemoteController.RemoteControllerInputDto;
+import nl.novi.techiteasyhw.dto.RemoteController.RemoteControllerOutputDto;
+import nl.novi.techiteasyhw.dto.Television.TelevisionOutputDto;
 import nl.novi.techiteasyhw.service.RemoteControllerService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
@@ -24,5 +25,15 @@ public class RemoteControllerController {
         RemoteControllerInputDto dto = remoteControllerService.addRemoteController(remoteControllerInputDto);
         return ResponseEntity.created(null).body(dto);
     }
+
+    @GetMapping('/remotecontrollers')
+    public ResponseEntity<List<RemoteControllerOutputDto>> getAllRemoteControllers () {
+        List<RemoteControllerOutputDto> dtos;
+        dtos = remoteControllerService.getAllRemoteControllers();
+        return ResponseEntity.ok().body(dtos);
+    }
+
+    @GetMapping ('/remotecontroller/{id}')
+    public ResponseEntity<RemoteControllerOutputDto> getRemoteController
 
 }
--- src/main/java/nl/novi/techiteasyhw/service/RemoteControllerService.java
+++ src/main/java/nl/novi/techiteasyhw/service/RemoteControllerService.java
@@ -1,8 +1,13 @@
 package nl.novi.techiteasyhw.service;
 
 import nl.novi.techiteasyhw.dto.RemoteController.RemoteControllerInputDto;
+import nl.novi.techiteasyhw.dto.RemoteController.RemoteControllerOutputDto;
+import nl.novi.techiteasyhw.model.RemoteController;
 import nl.novi.techiteasyhw.repository.RemoteContollerRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RemoteControllerService {
@@ -33,8 +38,26 @@ public class RemoteControllerService {
     }
 
     private RemoteControllerInputDto transferToDto(RemoteControllerInputDto remoteController) {
-
+        RemoteControllerInputDto dto = new RemoteControllerInputDto();
+        dto.setName(remoteController.getName());
+        dto.setBrand(remoteController.getBrand());
+        dto.setPrice(remoteController.getPrice());
+        dto.setBatteryType(remoteController.getBatteryType());
+        dto.setCompatibleWith(remoteController.getCompatibleWith());
+        dto.setOriginalStock(remoteController.getOriginalStock());
+    return dto;
     }
 
 
+    public List<RemoteControllerOutputDto> getAllRemoteControllers() {
+        List<RemoteController> remoteControllerList = repos.findAll();
+        List<RemoteControllerOutputDto> remoteControllerOutputDtoList = new ArrayList<>();
+
+        for (RemoteController remoteController : remoteControllerList) {
+            RemoteControllerOutputDto dto = transferToDto(remoteController);
+            remoteControllerOutputDtoList.add(dto);
+
+        }
+        return remoteControllerOutputDtoList;
+    }
 }
